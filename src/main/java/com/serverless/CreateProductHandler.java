22a4239ef83d10aaa73eaeb7527d250d01a2bac0
Igor Moiseyenko <imoiseyenko93@gmail.com>
2020-10-07 15:38:48 +0200

    CreateProduct Lambda function (handler/request/response).

---
+++ src/main/java/com/serverless/CreateProductHandler.java
@@ -0,0 +1,26 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import lombok.extern.log4j.Log4j;
+
+import java.util.UUID;
+
+import static com.serverless.Common.*;
+
+@Log4j
+public class CreateProductHandler implements RequestHandler<CreateProductRequest, CreateProductResponse> {
+
+    @Override
+    public CreateProductResponse handleRequest(CreateProductRequest request, Context context) {
+        log.info("Request: " + request);
+        final String productId = UUID.randomUUID().toString();
+        final Product product = Product.builder()
+                .id(productId)
+                .name(request.getName())
+                .price(request.getPrice())
+                .build();
+        PRODUCT_TABLE.putItem(product);
+        return CreateProductResponse.builder().id(productId).build();
+    }
+}
---
+++ src/main/java/com/serverless/CreateProductRequest.java
@@ -0,0 +1,18 @@
+package com.serverless;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateProductRequest implements Serializable {
+
+    private String name;
+    private double price;
+}
